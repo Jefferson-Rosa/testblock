1805aa823d47e2ec1bab7be29a1f81ffbe11bb2e
jeffryxd <81731978+jeffryxd@users.noreply.github.com>
2021-04-01 00:41:42 -0400

    Update testblock.js

--- testblock.js
+++ testblock.js
@@ -26,3 +26,20 @@ chrome.webRequest.onBeforeRequest.addListener(
   { urls: ["https://usher.ttvnw.net/api/channel/hls/*"] },
   ["blocking", "extraHeaders"]
 );
+
+----
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    ({ requestHeaders }) => {
+        for (const header of requestHeaders) {
+            if (header.name.toLowerCase() === "origin")
+                header.value = "https://player.twitch.tv";
+
+            if (header.name.toLowerCase() === "referer")
+                header.value = "https://player.twitch.tv/";
+        }
+        return { requestHeaders };
+    },
+    { urls: ["*://*.hls.ttvnw.net/*"] },
+    ["blocking", "requestHeaders", "extraHeaders"]
+);
+---
